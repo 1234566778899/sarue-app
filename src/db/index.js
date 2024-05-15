6ef7e51879c48c724e3281e13939eca4d385baca
u20211e436 <u20211e436@upc.edu.pe>
2024-05-14 21:31:20 -0500

    xx

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
   .connect(
-    //"mongodb+srv://ksanchezc2004:iJ6t89uRjGMAN3Qr@cluster0.xnnhm4s.mongodb.net/saruedb",
-    "mongodb://localhost:27017/saruedb",
+    "mongodb+srv://ksanchezc2004:iJ6t89uRjGMAN3Qr@cluster0.xnnhm4s.mongodb.net/saruedb",
+    //"mongodb://localhost:27017/saruedb",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
